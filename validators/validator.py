4d2800c9c2eed33ce190599f9b921a5c20cb2801
namoray <neuvaamor@proton.me>
2023-12-20 20:22:06 +0000

    increase sleep tiem again

--- validators/validator.py
+++ validators/validator.py
@@ -354,7 +354,7 @@ async def query_and_score_miners(
             )
 
             bt.logging.info("Bout to sleep for a bit, done scoring for now :)")
-            await asyncio.sleep(random.random() * 4)
+            await asyncio.sleep(random.random() * 60)
 
         except Exception as e:
             bt.logging.error(f"General exception: {e}\n{traceback.format_exc()}")
