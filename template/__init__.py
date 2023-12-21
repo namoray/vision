6b895c10da3eb5cb7c7fa9d8555f2400e6aa4147
namoray <neuvaamor@proton.me>
2023-12-21 18:36:13 +0000

    actually updated the version, woops

--- template/__init__.py
+++ template/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 PROJECT_NAME = "vision"
