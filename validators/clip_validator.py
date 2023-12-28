b70f46c8ba1358f1612f4effa346fe6892ded36e
namoray <neuvaamor@proton.me>
2023-12-28 15:38:16 +0000

    remove one ugly log

--- validators/clip_validator.py
+++ validators/clip_validator.py
@@ -103,8 +103,6 @@ class ClipValidator(BaseValidator):
         expected_response = await self.get_expected_image_embeddings(image_b64s)
         score = self.score_dot_embeddings(expected_response, response_synapse.image_embeddings)
 
-        bt.logging.error("score of response: {} for uid  {}".format(score, uid))
-
         del response_synapse.image_embeddings
 
         return (uid, score)
