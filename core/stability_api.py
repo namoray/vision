89d562b0faf5d869988204308f5cdda06213b51d
namoray <neuvaamor@proton.me>
2024-01-04 14:34:37 +0000

    Removed some logging

--- core/stability_api.py
+++ core/stability_api.py
@@ -112,9 +112,6 @@ async def generate_images_from_image(
     engine_id: str = cst.DEFAULT_ENGINE,
 ) -> List[str]:
     image_resized = resize_image(init_image)
-    bt.logging.debug("Guess who just resized an image!")
-    bt.logging.debug(f"Image strength: {image_strength}")
-    bt.logging.debug(f"cfg_scale: {cfg_scale}")
     data = {
         "init_image": base64.b64decode(image_resized),
         "image_strength": str(image_strength),
