55262cb1f39ff98620f7370f705bf93c04ea125b
namoray <neuvaamor@proton.me>
2024-01-21 16:49:04 +0000

    Checking for stake and requiring at least 5000

--- miners/miner.py
+++ miners/miner.py
@@ -349,9 +349,7 @@ class MinerBoi:
             bt.logging.trace(f"Blacklisting unrecognized hotkey {synapse.dendrite.hotkey}")
             return True, synapse.dendrite.hotkey
 
-        bt.logging.info("here!")
         stake = self.metagraph.S[self.metagraph.hotkeys.index(synapse.dendrite.hotkey)]
-        bt.logging.info(f"Stake of {synapse.dendrite.hotkey} is {stake}")
         if stake < 5000:
             bt.logging.trace(f"Blacklisting hotkey, stake too low! {synapse.dendrite.hotkey}")
             return True, synapse.dendrite.hotkey
