d52f15462c2f6de808b5eba52a3a85efaf91b4f5
namoray <neuvaamor@proton.me>
2024-02-19 11:49:40 +0000

    sleeps

--- validation/run_all_servers.sh
+++ validation/run_all_servers.sh
@@ -2,8 +2,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-"$SCRIPT_DIR/api_server.sh" "$@"
-
-"$SCRIPT_DIR/checking_server.sh"
-
 "$SCRIPT_DIR/safety_server.sh"
+sleep 30
+"$SCRIPT_DIR/checking_server.sh"
+sleep 30
+
+"$SCRIPT_DIR/api_server.sh" "$@"
