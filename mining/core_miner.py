112cf8bbac65b03fe5b017edbc1322fa3215e7ae
namoray <neuvaamor@proton.me>
2024-03-06 20:40:24 +0000

    min validator stake to 5000

--- mining/core_miner.py
+++ mining/core_miner.py
@@ -17,7 +17,7 @@ T = TypeVar("T", bound=bt.Synapse)
 metagraph = None
 
 requests_from_each_validator = {}
-MIN_VALIDATOR_STAKE = 0
+MIN_VALIDATOR_STAKE = 5000
 
 def base_blacklist(synapse: T) -> Tuple[bool, str]:
     if synapse.dendrite.hotkey not in metagraph.hotkeys:
