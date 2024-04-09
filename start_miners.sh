4bda3b7a4f5e5b30b8ce012aa4d1c33fa48bb02d
namoray <neuvaamor@proton.me>
2024-04-09 17:32:13 +0100

    start mine has 51 in script, it should not have it

--- start_miners.sh
+++ start_miners.sh
@@ -4,6 +4,6 @@ for env_file in .*.env; do
     if ! grep -q 'IS_VALIDATOR=True' $env_file; then
         hotkey=$(basename $env_file .env | cut -c 2-)
         pm2 delete mining_server_$hotkey
-        pm2 start --name mining_server_$hotkey mining/proxy/run_miner.py --interpreter python3 -- --netuid 51 --logging.debug --env_file $env_file
+        pm2 start --name mining_server_$hotkey mining/proxy/run_miner.py --interpreter python3 -- --logging.debug --env_file $env_file
     fi
 done
