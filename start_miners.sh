681fa7053a15e05edb9bf2b5ebcbca5542f5ff0f
namoray <neuvaamor@proton.me>
2024-04-09 11:26:46 +0100

    Start miners

---
+++ start_miners.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for env_file in .*.env; do
+    if ! grep -q 'IS_VALIDATOR=True' $env_file; then
+        hotkey=$(basename $env_file .env | cut -c 2-)
+        pm2 delete mining_server_$hotkey
+        pm2 start --name mining_server_$hotkey mining/proxy/run_miner.py --interpreter python3 -- --netuid 51 --logging.debug --env_file $env_file
+    fi
+done
