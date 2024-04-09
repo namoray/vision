ae358a80f4d344ababc5d0ebe94cec0299e87af3
namoray <neuvaamor@proton.me>
2024-04-09 08:38:41 +0100

    Python instead of python£

--- launch_validators.sh
+++ launch_validators.sh
@@ -2,12 +2,10 @@
 
 set -x
 
-. venv/bin/activate
-
 for env_file in .*.env; do
     if grep -q 'IS_VALIDATOR=True' $env_file; then
         hotkey=$(basename $env_file .env | cut -c 2-)
         # pm2 delete validating_server_$hotkey
-        pm2 start validation/proxy/api_server/asgi.py --name validating_server_$hotkey --interpreter python3 -- --env_file $env_file 2> errorlog.txt
+        pm2 start validation/proxy/api_server/asgi.py --name validating_server_$hotkey --interpreter python -- --env_file $env_file 2> errorlog.txt
     fi
 done
