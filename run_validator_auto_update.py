591b00258152aaa643777a06ed99301af432dfbe
namoray <neuvaamor@proton.me>
2024-04-12 17:49:15 +0100

    Autoupdater

--- run_validator_auto_update.py
+++ run_validator_auto_update.py
@@ -3,35 +3,35 @@ import subprocess
 import time
 
 
-def should_update_local(local_tag, remote_tag):
-    if remote_tag[0] == local_tag[0]:
-        return remote_tag != local_tag
-    return False
+def should_update_local(local_commit, remote_commit):
+    return local_commit != remote_commit
 
 
 process = subprocess.Popen(["./launch_validators.sh"], stdout=subprocess.PIPE)
+time.sleep(10)
 
 
 def run_auto_updater():
     while True:
-        local_tag = subprocess.getoutput("git describe --abbrev=0 --tags")
+        local_commit = subprocess.getoutput("git rev-parse HEAD")
         os.system("git fetch")
-        remote_tag = subprocess.getoutput("git describe --tags `git rev-list --topo-order --tags HEAD --max-count=1`")
+        remote_commit = subprocess.getoutput("git rev-parse origin/HEAD")
 
-        if should_update_local(local_tag, remote_tag):
+        if should_update_local(local_commit, remote_commit):
             print("Local repo is not up-to-date. Updating...")
-            reset_cmd = "git reset --hard " + remote_tag
+            reset_cmd = "git reset --hard " + remote_commit
             process = subprocess.Popen(reset_cmd.split(), stdout=subprocess.PIPE)
             output, error = process.communicate()
 
             if error:
                 print("Error in updating:", error)
             else:
-                print("Updated local repo to latest version: {}", format(remote_tag))
+                print("Updated local repo to latest version: {}", format(remote_commit))
 
                 print("Running the autoupdate steps...")
                 # Trigger shell script. Make sure this file path starts from root
                 subprocess.call(["./autoupdate_validator_steps.sh"], shell=True)
+                time.sleep(20)
 
                 print("Finished running the autoupdate steps! Ready to go 😎")
 
