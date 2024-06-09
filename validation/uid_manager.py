d221f3d537bb17f7bdd8012a870100325f050d07
namoray <neuvaamor@proton.me>
2024-06-09 09:21:50 +0000

    Lower loggng

--- validation/uid_manager.py
+++ validation/uid_manager.py
@@ -120,9 +120,11 @@ class UidManager:
         i = 0
         tasks_in_progress = []
         while uid_record.synthetic_requests_still_to_make > 0:
-            bt.logging.debug(
-                f"synthetic requests still to make: {uid_record.synthetic_requests_still_to_make} on iteration {i} for uid {uid_record.axon_uid} and task {task}"
-            )
+
+            if uid_record.synthetic_requests_still_to_make % 50 == 0:
+                bt.logging.debug(
+                    f"synthetic requests still to make: {uid_record.synthetic_requests_still_to_make} on iteration {i} for uid {uid_record.axon_uid} and task {task}"
+                )
 
             if uid_record.consumed_volume >= volume_to_score:
                 break
