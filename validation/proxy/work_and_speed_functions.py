e9cdf07a803470e2a3a74a78dcc11a5c452e3cb5
namoray <neuvaamor@proton.me>
2024-06-13 13:01:15 +0100

    run all tasks

--- validation/proxy/work_and_speed_functions.py
+++ validation/proxy/work_and_speed_functions.py
@@ -23,23 +23,14 @@ def _calculate_speed_modifier(time_per_unit: float, config: TaskConfig) -> float
 
     assert variance > 0
 
-    bt.logging.warning(
-        f"\nGetting the speed for task: {config.task} with time_per_unit time: {time_per_unit}."
-        f"\nMean: {mean}\nVariance: {variance}"
-        f"\nBelow mean exponent: {BELOW_MEAN_EXPONENT}"
-        f"\nMax speed bonus: {MAX_SPEED_BONUS}"
-    )
-
     if time_per_unit <= mean:
         # y = 1 + (M - 1) * (b - x)^c / b^c
         speed_modifier = 1 + (MAX_SPEED_BONUS - 1) * ((mean - time_per_unit) ** BELOW_MEAN_EXPONENT) / (
             mean**BELOW_MEAN_EXPONENT
         )
-        bt.logging.warning(f"Speed modifier in the if: {speed_modifier}.")
     else:
         # y = e^((b - x) * v)
         speed_modifier = math.exp((mean - time_per_unit) * variance)
-        bt.logging.warning(f"Speed modifier in the else: {speed_modifier}.")
 
     return speed_modifier
 
