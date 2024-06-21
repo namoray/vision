d92ae83d13431cbd80812b1bf2256dc1605723aa
namoray <neuvaamor@proton.me>
2024-06-21 13:11:38 +0100

    Manual weight set description

--- manual_weight_set.py
+++ manual_weight_set.py
@@ -2,10 +2,10 @@
 This is a utility script for manually setting weights in the case of any issues
 Use cases:
 - Testing weight setting to see scores and help debug
-- Emergencies if weight setting in the vali is nothing working [not recommended]
+- Emergencies if weight setting in the vali is facing issues - but the proxy is still working as expected [not recommended]
 
-NOTE: this is not artificial weights, it uses real values obtained by the validator proxy only.
-It's just taking that part of the code, and making it runnable
+NOTE: this is not artificial weights / weight copying, it uses real values obtained by the validator proxy only.
+It's just taking that part of the code, and making it independently runnable
 
 Usage:
 python manually_set_weights.py --env_file {youvr_vali_hotkey_env_file_here}
