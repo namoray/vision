789b92d562f6709465165cd3bce9965861a91fc4
namoray <neuvaamor@proton.me>
2024-06-23 11:23:48 +0100

    update :D

--- validation/db/db_management.py
+++ validation/db/db_management.py
@@ -81,7 +81,9 @@ class DatabaseManager:
         checking_data, miner_hotkey = row
         checking_data_loaded = json.loads(checking_data)
 
-        cursor.execute(sql.delete_specific_task(), (task.value, checking_data))
+
+        # TODO: re-enable
+        # cursor.execute(sql.delete_specific_task(), (task.value, checking_data))
         self.conn.commit()
 
         return checking_data_loaded, miner_hotkey
